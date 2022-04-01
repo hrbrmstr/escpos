e93705c055c3b0cb49b1a5d46521c54fee0e12c5
hrbrmstr <bob@rud.is>
2022-04-01 09:27:25 -0400

    initial commit

--- R/ggpos-package.R
+++ R/ggpos-package.R
@@ -1,9 +1,15 @@
-#' ...
-#' 
+#' Print {ggplot2} or {grid} Objects to ESC/POS Compatible Network Devices
+#'
+#' ESC/POS devices, such as receipt printers, have the ability to print their own form of
+#' raster bitmap graphics. Tools are provided to turn {ggplot2} or {grid} objects into such raster
+#' bitmaps and print them to ESC/POS compatible devices.
+#'
 #' @md
 #' @name ggpos
 #' @keywords internal
 #' @author Bob Rudis (bob@@rud.is)
-#' @import httr
-#' @importFrom jsonlite fromJSON
+#' @importFrom ggplot2 last_plot ggsave
+#' @importFrom stringi stri_read_raw
+#' @useDynLib ggpos, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
 "_PACKAGE"
